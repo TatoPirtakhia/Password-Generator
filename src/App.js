ec98f03171ec1086d8de935db7eca1fc67b74a53
TatoPirtakhia <tarassatato@gmail.com>
2023-03-30 19:20:32 +0400

    mobile styles is done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,102 @@
 import "./App.css";
 import { useState } from "react";
 function App() {
-  const {range,setRange} = useState(10);
-  
+  const [password,setPassword] = useState("P4$5W0rD!")
+  const [output,setOutput] = useState(false)
+  const [range, setRange] = useState(10);
+  const [one, setone] = useState(false);
+  const [two, setTwo] = useState(false);
+  const [three, setThree] = useState(false);
+  const [four, setFour] = useState(false);
+
+  const [strengthBoxOne, setStrengthBoxOne] = useState(false);
+  const [strengthBoxTwo, setStrengthBoxTwo] = useState(false);
+  const [strengthBoxThree, setStrengthBoxThree] = useState(false);
+  const [strengthBoxFour, setStrengthBoxFour] = useState(false);
+  const [strength, setStrength] = useState("");
+
+  const [strongCOlor,setStrongCOlor] = useState()
+
   const changeWidth = (event) => {
     setRange(event.target.value);
-  }
-  
-  
+  };
+
+  const Generate = () => {
+    setPassword('qwertyuiopasdfghjklz')
+    setOutput(true)
+    setStrengthBoxOne(false);
+    setStrengthBoxTwo(false);
+    setStrengthBoxThree(false);
+    setStrengthBoxFour(false);
+    setStrongCOlor("")
+    let count = 0;
+    if (one) count++;
+    if (two) count++;
+    if (three) count++;
+    if (four) count++;
+    
+    if (count == 1) {
+      setStrength("TOO WEAK!");
+      setStrengthBoxOne(true);
+      setStrongCOlor("redBox")
+    }
+    if (count == 2) {
+      setStrength("WEAK");
+      setStrengthBoxOne(true);
+      setStrengthBoxTwo(true);
+      setStrongCOlor("orangeBox")
+    }
+    if (count == 3) {
+      setStrength("MEDIUM");
+      setStrengthBoxOne(true);
+      setStrengthBoxTwo(true);
+      setStrengthBoxThree(true);
+      setStrongCOlor("yellowBox")
+    }
+    if (count == 4) {
+      setStrength("STRONG");
+      setStrengthBoxOne(true);
+      setStrengthBoxTwo(true);
+      setStrengthBoxThree(true);
+      setStrengthBoxFour(true);
+      setStrongCOlor("greenBox")
+    }
+    let string = ''
+    let countLength = 0;
+    while(countLength != range){
+      countLength++;
+      if (one){
+        const indx = Math.random()
+      }
+    }
+  };
+  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const symbols = ["!", "@", "#", "$", "%", "^", "&", "*"];
+  const UpperCaseletters = (() => {
+    const caps = [...Array(26)].map((value, i) => String.fromCharCode(i + 65));
+    return caps;
+  })();
+
+  const LowerCaseletters = (() => {
+    return UpperCaseletters.map((letter) => letter.toLowerCase());
+  })();
+  const boxOneCheck = (event) => {
+    setone((one) => !one);
+  };
+  const boxTwoCheck = () => {
+    setTwo((two) => !two);
+  };
+  const boxThreeCheck = () => {
+    setThree((three) => !three);
+  };
+  const boxFourCheck = () => {
+    setFour((four) => !four);
+  };
   return (
-      <div className="App">
+    <div className="App">
       <h1 className="title">Password Generator</h1>
       <div className="password_copy">
-        <span className="passwordOutput"></span>
+        <span className={output ?"passwordOutput":"changePassword"}>{password}</span>
         <svg width="21" height="24" xmlns="http://www.w3.org/2000/svg">
           <path
             d="M20.341 3.091 17.909.659A2.25 2.25 0 0 0 16.319 0H8.25A2.25 2.25 0 0 0 6 2.25V4.5H2.25A2.25 2.25 0 0 0 0 6.75v15A2.25 2.25 0 0 0 2.25 24h10.5A2.25 2.25 0 0 0 15 21.75V19.5h3.75A2.25 2.25 0 0 0 21 17.25V4.682a2.25 2.25 0 0 0-.659-1.591ZM12.469 21.75H2.53a.281.281 0 0 1-.281-.281V7.03a.281.281 0 0 1 .281-.281H6v10.5a2.25 2.25 0 0 0 2.25 2.25h4.5v1.969a.282.282 0 0 1-.281.281Zm6-4.5H8.53a.281.281 0 0 1-.281-.281V2.53a.281.281 0 0 1 .281-.281H13.5v4.125c0 .621.504 1.125 1.125 1.125h4.125v9.469a.282.282 0 0 1-.281.281Zm.281-12h-3v-3h.451c.075 0 .147.03.2.082L18.667 4.6a.283.283 0 0 1 .082.199v.451Z"
@@ -25,38 +109,85 @@ function App() {
           <p className="main_title">Character Length</p>
           <span className="valueOfRange">{range}</span>
         </div>
-        <input type="range" className="range" min="1" max="20" step={1} value={range}  onChange={changeWidth}/>
+        <input
+          type="range"
+          className="range"
+          min="1"
+          max="20"
+          step={1}
+          value={range}
+          onChange={changeWidth}
+        />
         <div className="checkBoxes">
           <div className="boxContainer">
-            <div className="box"></div>
+            <div className={one ? "box_check" : "box"} onClick={boxOneCheck}>
+              <svg width="14" height="12" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  stroke="#18171F"
+                  stroke-width="3"
+                  fill="none"
+                  d="M1 5.607 4.393 9l8-8"
+                />
+              </svg>
+            </div>
             <p className="main_title">Include Uppercase Letters</p>
           </div>
           <div className="boxContainer">
-            <div className="box"></div>
+            <div className={two ? "box_check" : "box"} onClick={boxTwoCheck}>
+              <svg width="14" height="12" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  stroke="#18171F"
+                  stroke-width="3"
+                  fill="none"
+                  d="M1 5.607 4.393 9l8-8"
+                />
+              </svg>
+            </div>
             <p className="main_title"> Include Lowercase Letters</p>
           </div>
           <div className="boxContainer">
-            <div className="box"></div>
+            <div
+              className={three ? "box_check" : "box"}
+              onClick={boxThreeCheck}
+            >
+              <svg width="14" height="12" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  stroke="#18171F"
+                  stroke-width="3"
+                  fill="none"
+                  d="M1 5.607 4.393 9l8-8"
+                />
+              </svg>
+            </div>
             <p className="main_title">Include Numbers</p>
           </div>
           <div className="boxContainer">
-            <div className="box"></div>
+            <div className={four ? "box_check" : "box"} onClick={boxFourCheck}>
+              <svg width="14" height="12" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  stroke="#18171F"
+                  stroke-width="3"
+                  fill="none"
+                  d="M1 5.607 4.393 9l8-8"
+                />
+              </svg>
+            </div>
             <p className="main_title">Include Symbols </p>
           </div>
         </div>
         <div className="strength_medium">
           <p className="strength">STRENGTH</p>
           <div className="medium_box">
-            <p className="medium">MEDIUM</p>
+            <p className="medium">{strength}</p>
             <div className="stength_boxes">
-              <div className="strength_box"></div>
-              <div className="strength_box"></div>
-              <div className="strength_box"></div>
-              <div className="strength_box"></div>
+              <div className={strengthBoxOne ? strongCOlor:"strength_box"}></div>
+              <div className={strengthBoxTwo ? strongCOlor:"strength_box"}></div>
+              <div className={strengthBoxThree ? strongCOlor:"strength_box"}></div>
+              <div className={strengthBoxFour ? strongCOlor:"strength_box"}></div>
             </div>
           </div>
         </div>
-        <button>
+        <button onClick={Generate}>
           <p className="generate">GENERATE</p>
           <svg width="12" height="12" xmlns="http://www.w3.org/2000/svg">
             <path
